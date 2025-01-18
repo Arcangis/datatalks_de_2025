67a00796598040b57f6648b668522edcb88762fb
Arcangis <thaliakj@gmail.com>
2025-01-18 13:12:36 -0300

    add: skip_final_snapshot

--- homework_1/infra/main.tf
+++ homework_1/infra/main.tf
@@ -24,10 +24,11 @@ resource "aws_s3_bucket_lifecycle_configuration" "datatalks_de_lifecycle" {
 }
 
 resource "aws_redshift_cluster" "datatalks_de_redshift" {
-  cluster_identifier = var.redshift_cluster
-  database_name      = var.redshift_database
-  master_username    = var.redshift_username
-  master_password    = var.redshift_password
-  node_type          = "dc2.large"
-  cluster_type       = "single-node"
+  cluster_identifier  = var.redshift_cluster
+  database_name       = var.redshift_database
+  master_username     = var.redshift_username
+  master_password     = var.redshift_password
+  skip_final_snapshot = true 
+  node_type           = "dc2.large"
+  cluster_type        = "single-node"
 }
